26fd7d476baec70e5713386dcdf4f2c331700991
pflannery <pflannery@users.noreply.github.com>
2014-07-10 07:02:20 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,109 @@
+# Angular JSDom Renderer
+
+[![Build Status](https://secure.travis-ci.org/pflannery/angular-jsdom-renderer.png?branch=master)](http://travis-ci.org/pflannery/angular-jsdom-renderer "Check this project's build status on TravisCI")
+[![NPM version](https://badge.fury.io/js/angular-jsdom-renderer.png)](https://npmjs.org/package/angular-jsdom-renderer "View this project on NPM")
+[![Dependency Status](https://gemnasium.com/pflannery/angular-jsdom-renderer.png)](https://gemnasium.com/pflannery/angular-jsdom-renderer)
+[![Gittip donate button](http://img.shields.io/gittip/pflannery.png)](https://www.gittip.com/pflannery/ "Donate weekly to this project using Gittip")
+[![Analytics](https://ga-beacon.appspot.com/UA-47157500-1/angular-jsdom-renderer/readme)](https://github.com/pflannery/angular-jsdom-renderer)
+
+Helps generate render angular pages on the server-side.
+
+## Installation
+
+    npm install angular-jsdom-renderer
+
+## Usage
+
+```javascript
+
+// render using an interval poll selector
+var renderer = require('angular-jsdom-renderer');
+
+renderer.render({
+	/* url to retrieve the html from */
+	url: 'http://localhost:7559/',
+
+	/* poll this selector until a match is found */
+	pollSelector: 'body[data-content-loaded=true]. default undefined',
+	
+	/* how often to poll the given selector. default 500 */
+	pollSelectorMs: 100,
+	
+	/* how long to wait to give up polling selector. default 60000 */
+	timeoutMs: 60000,
+	
+	/* complete callback. this = config */
+	done: function(errors, compiledHtml) {
+		if (errors) {
+			console.log(errors);
+			return;
+		}
+		
+		console.log(compiledHtml);
+	}
+});
+
+// render using a timeout only
+var renderer = require('angular-jsdom-renderer');
+
+renderer.render({
+	/* url to retrieve the html from */
+	url: 'http://localhost:7559/',
+	
+	/* how long to wait until rendering the content. default 60000 */
+	timeoutMs: 5000,
+	
+	/* complete callback. this = config */
+	done: function(errors, compiledHtml) {
+		if (errors) {
+			console.log(errors);
+			return;
+		}
+		
+		console.log(compiledHtml);
+	}
+});
+
+```
+
+
+## API
+
+######angular-jsdom-renderer module
+
+```javascript
+
+	/* renders the give url or file or html string */
+	
+	render({
+		/* raw html. optional but must provide either url|file|html */
+		html: '',
+		/* raw file. optional but must provide either url|file|html  */
+		file: config.file,
+		/* raw url. optional but must provide either url|file|html  */
+		url: '',
+		/* injects script urls in to the html to be rendered */
+		scripts: [],
+		/* injects script statements in to the html to be rendered */
+		src: [],
+		/* see jsdom for more info - https://github.com/tmpvar/jsdom#how-it-works */
+		features: config.features,
+		/* see jsdom for more info - https://github.com/tmpvar/jsdom#doneerrors-window */
+		done: function(jsdomErrors, window){},
+		/* polls a selector until a match is found and then passes the rendered html to the config.done callback */
+		pollSelector: 'body[data-content-loaded=true]'. default '',
+		/* how often to poll the given selector. default 500 */
+		pollSelectorMs: 500,
+		/* how long to wait to give up polling selector. default 60000 */
+		timeoutMs: 60000,
+	});
+	
+```
+
+## Contributing
+Feel free to submit ideas and issues.
+
+## License
+Licensed under the incredibly [permissive](http://en.wikipedia.org/wiki/Permissive_free_software_licence) [MIT License](http://creativecommons.org/licenses/MIT/)
+<br/>Copyright &copy; 2014+ Stringz Solutions Ltd
+<br/>Copyright &copy; 2014+ [Peter Flannery](http://github.com/pflannery)
---
+++ src/angular-jsdom-renderer.js
@@ -0,0 +1,217 @@
+(function(exports){
+	'use strict';
+	
+	function destroyAngularWindow(window) {
+		
+		// get the rootScope and destroy it before closing the window otherwise stack error occurs
+		var rootScope = window.angular.element(window.document).scope();
+		
+		//if (rootScope && rootScope.$destroy) {
+		rootScope.$destroy();
+		//}
+		
+		// gracefully close the window to release this process
+		window.close();
+		
+	}
+	
+	function stopPollTimeout(timeoutId) {
+		if (timeoutId) {
+			clearTimeout(timeoutId);
+		}
+	}
+	
+	function stopPollInterval(intervalId) {
+		if (intervalId) {
+			clearInterval(intervalId);
+		}
+	}
+	
+	function runPollTimeout(window, document, angular, config) {
+		
+		var pollTimeoutMs = config.timeoutMs || 3000,
+			timeoutId = setTimeout(
+				function(){
+					// store the rendered html
+					var compiledHtml = document.outerHTML;
+					
+					// kill the timeout
+					clearTimeout(timeoutId);
+					
+					// kill the angular window 
+					destroyAngularWindow(window);
+					
+					// supply the compiled html to the done fn
+					config.done(null, compiledHtml);
+				}
+			, pollTimeoutMs);
+		
+		return timeoutId;
+	};
+	
+	function runPollInterval(window, document, angular, config) {
+		
+		var pollSelectorMs = config.pollSelectorMs || 500,
+			timeoutId,
+			intervalId = setInterval(
+				function(){
+					var documentElement,
+						pollElement = [];
+					
+					try {
+						documentElement = document.querySelectorAll(config.pollSelector);
+						
+						// poll for an element using the poll selector
+						pollElement = angular.element(documentElement);
+					}catch(err) {
+						throw new Error("Poll selector failed: " + err.message);
+					}
+					
+					// if the element length is not 0 then kill this poll
+					if (pollElement.length != 0) {
+						// store the rendered html
+						var compiledHtml = document.outerHTML;
+						
+						// kill this interval
+						clearInterval(intervalId);
+						
+						// kill the timeout
+						clearTimeout(timeoutId);
+						
+						// kill the angular window 
+						destroyAngularWindow(window);
+						
+						// supply the compiled html to the done fn
+						config.done(null, compiledHtml);
+					}
+					
+				}
+			, pollSelectorMs);
+		
+		//run timeout fn to handle cases where selector is never found within the timeout limit
+		timeoutId = setTimeout(function() {
+			
+			// kill the interval
+			clearInterval(intervalId);
+			
+			// kill the angular window 
+			destroyAngularWindow(window);
+			
+			// send the timeout error
+			config.done([new Error('The poll selector timed out.')], null);
+			
+		}, config.timeoutMs || 60000);
+		
+		// return the id
+		return intervalId;
+	};
+	
+	
+	//overload: config object
+	exports.render = function(config) {
+		var config,
+			runPoll = runPollTimeout,
+			stopPoll = stopPollTimeout;
+		
+		// ensure we have an object as the first argument
+		if (typeof config !== 'object') {
+			throw new Error("No config was supplied");
+		}
+		
+		// check for a done callback
+		if (typeof config.done !== 'function') {
+			throw Error("You have not supplied a config.done function.");
+		} else {
+			config.done.bind(config);
+		}
+		
+		// ensure we have html to render
+		if (config.url === undefined 
+			&& config.html === undefined
+			&& config.file === undefined) {
+				config.done(new Error("You must supply either a config.url, config.file or config.html value."), null);
+				return;
+		}
+		
+		// set default features if not present
+		if (config.features === undefined) {
+			config.features = {
+				FetchExternalResources   : ['script'], 
+				ProcessExternalResources : ['script'],
+				MutationEvents           : '2.0'
+			};
+		}
+		
+		// decides which poll to run. [timeout|interval]
+		if (!config.pollSelector) {
+			runPoll = runPollTimeout;
+			stopPoll = stopPollTimeout;
+		}
+		else {
+			runPoll = runPollInterval;
+			stopPoll = stopPollInterval;
+		}
+		
+		var jsdom = require("jsdom"),
+			document = jsdom.env( 
+			{
+				/* raw html. optional but must provide either url|file|html */
+				html: config.html,
+				/* raw file. optional but must provide either url|file|html  */
+				file: config.file,
+				/* raw url. optional but must provide either url|file|html  */
+				url: config.url,
+				/* injects script urls in to the html to be rendered */
+				scripts: config.scripts,
+				/* injects script statements in to the html to be rendered */
+				src: config.src,
+				/* see jsdom for more info - https://github.com/tmpvar/jsdom#how-it-works */
+				features: config.features,
+				/* see jsdom for more info - https://github.com/tmpvar/jsdom#doneerrors-window */
+				done: function(jsdomErrors, window) {
+					if (jsdomErrors) {
+						config.done(jsdomErrors, null);
+						return;
+					}
+					
+					var pollId,
+						document = window.document;
+					
+					document.addEventListener("error", function(errorMsg, url, lineNumber){
+						stopPoll(pollId);
+						config.done([new Error(errorMsg, url, lineNumber)], null);
+					}, false);
+					
+					document.addEventListener("load", function(){
+						var angular = window.angular;
+						
+						if (!angular) {
+							config.done([new Error("AngularJs library was not detected.")], null);
+							return;
+						}
+						
+						pollId = runPoll(window, document, angular, config);
+						
+					}, false);
+					
+				}
+			});
+	}
+	
+	exports.renderMany = function (configArray) {
+		var config;
+		
+		// ensure we have an array
+		if (! Array.isArray(configArray)) {
+			throw new Error("No config array was supplied");
+		}
+		
+		// queue each render
+		configArray.forEach(function(config) {
+			exports.render(config);
+		});
+		
+	}
+	
+	
+}(module.exports));
