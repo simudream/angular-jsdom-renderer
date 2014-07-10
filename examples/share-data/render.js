1fffb876169a41849d24d89507c074ca2b4c744b
pflannery <pflannery@users.noreply.github.com>
2014-07-10 08:55:23 +0800

    adds an example
    bumped version

---
+++ examples/share-data/render.js
@@ -0,0 +1,29 @@
+// render using an interval poll selector
+var renderer = require('angular-jsdom-renderer');
+var path = require("path");
+var asset = path.join.bind(null, __dirname);
+//console.log(asset('controllers.js'))
+
+renderer.render({
+
+    /* url to retrieve the html from */
+    file: asset('index.html'),
+	
+    /* poll this selector until a match is found */
+    pollSelector: 'body[data-ready=true]',
+	
+	/* share data to the renderer's window object */
+	global: {
+		message: 123
+	},
+	
+    /* complete callback. this = config */
+    done: function(errors, compiledHtml) {
+        if (errors) {
+            console.log("errors", errors);
+            
+        }
+
+        console.log("compiled", compiledHtml);
+    }
+});
